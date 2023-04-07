ce78e82eb1e07ed3acc879474624e9f75626ee28
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-04-07 15:22:20 +0900

    update

--- src/components/pages/Editor.tsx
+++ src/components/pages/Editor.tsx
@@ -42,14 +42,20 @@ const initialNodes: Node<NodeDataType>[] = [
   },
   {
     id: '2',
-    data: { label: 'move', name: 'move', color: '#FF5660' },
+    data: { label: 'down_arm', name: 'down_arm', color: '#FF5660' },
     position: { x: 5, y: 100 },
   },
   {
     id: '3',
-    data: { label: 'move_boom', name: 'move_boom', color: '#FF5660' },
+    data: { label: 'move', name: 'move', color: '#FF5660' },
     position: { x: 5, y: 200 },
   },
+  {
+    id: '4',
+    data: { label: 'up_arm', name: 'up_arm', color: '#FF5660' },
+    position: { x: 5, y: 200 },
+  },
+
 
 ]
 
@@ -57,6 +63,7 @@ const initialNodes: Node<NodeDataType>[] = [
 const initialEdges: Edge[] = [
   { id: 'e1-2', source: '1', target: '2' },
   { id: 'e2-3', source: '2', target: '3' },
+  { id: 'e3-4', source: '3', target: '4' },
 ]
 
 const fitViewOptions: FitViewOptions = {
