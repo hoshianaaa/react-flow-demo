03e586f0113590c11ca2a913f129950bace218f5
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-04-08 18:41:21 +0900

    update

--- src/components/pages/Editor.tsx
+++ src/components/pages/Editor.tsx
@@ -12,8 +12,6 @@ import { Ros, Topic, Message } from 'roslib';
 import TreeMenu from 'react-simple-tree-menu'
 import 'node_modules/react-simple-tree-menu/dist/main.css';
 //import 'src/components/pages/treemenu.css';
-import ColorSelectorNode from './ColorSelectorNode';
-import ResizableNode from './ResizableNode';
 
 
 
