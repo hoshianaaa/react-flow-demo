b59e925e4c2e63f796c7729b58600acc68835378
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-04-11 15:06:43 +0900

    update

--- src/components/organisms/EditorSidePanel.tsx
+++ src/components/organisms/EditorSidePanel.tsx
@@ -67,7 +67,7 @@ export const EditorSidePanel = ({ settings, node }: EditorSidePanelProps) => {
   // tailwindcss border color 一覧: https://tailwindcss.com/docs/border-color 
   // tailwindcss border width: https://tailwindcss.com/docs/border-width
 
-    <div className="w-1/4 h-5/6 bg-fuchsia-300 bg-opacity-20 absolute bottom-10 right-4 z-50 rounded-2xl drop-shadow-md text-fuchsia-600 border-fuchsia-600 border">
+    <div className="max-w-xs w-1/4 h-5/6 bg-fuchsia-300 bg-opacity-20 absolute bottom-10 right-4 z-50 rounded-2xl drop-shadow-md text-fuchsia-600 border-fuchsia-600 border">
       <div
         className="rounded-t-2xl h-4"
         //style={{ backgroundColor: node.data.color }}
